1233f1efd14f174579adb56d8206f18df3511149
sbjwin <sbjwin4271@gmail.com>
2025-04-02 00:49:17 +0900

    cicd java버전 수정
    Signed-off-by: sbjwin <sbjwin4271@gmail.com>

---
+++ src/main/java/me/baekjin/springbootdeveloper/dto/UpdateArticleRequest.java
@@ -0,0 +1,13 @@
+package me.baekjin.springbootdeveloper.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class UpdateArticleRequest {
+    private String title;
+    private String content;
+}
---
+++ src/main/java/me/baekjin/springbootdeveloper/service/BlogService.java
@@ -0,0 +1,59 @@
+package me.baekjin.springbootdeveloper.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import me.baekjin.springbootdeveloper.domain.Article;
+import me.baekjin.springbootdeveloper.dto.AddArticleRequest;
+import me.baekjin.springbootdeveloper.dto.UpdateArticleRequest;
+import me.baekjin.springbootdeveloper.repository.BlogRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class BlogService {
+
+    private final BlogRepository blogRepository;
+
+    public Article save(AddArticleRequest request, String userName) {
+        return blogRepository.save(request.toEntity(userName));
+    }
+
+    public List<Article> findAll() {
+        return blogRepository.findAll();
+    }
+
+    public Article findById(long id) {
+        return blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found : " + id));
+    }
+
+    public void delete(long id) {
+        Article article = blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found : " + id));
+
+        authorizeArticleAuthor(article);
+        blogRepository.delete(article);
+    }
+
+    @Transactional
+    public Article update(long id, UpdateArticleRequest request) {
+        Article article = blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found : " + id));
+
+        authorizeArticleAuthor(article);
+        article.update(request.getTitle(), request.getContent());
+
+        return article;
+    }
+
+    private static void authorizeArticleAuthor(Article article) {
+        String userName = SecurityContextHolder.getContext().getAuthentication().getName();
+        if (!article.getAuthor().equals(userName)) {
+            throw new IllegalArgumentException("not authorized");
+        }
+    }
+
+}
